1701796b126254c58ab3ccc767d67c1ff646a791
巴基亮 <pakistanleong@bajiliangde-MacBook-Pro.local>
2018-07-01 18:32:23 +0800

    console.log(req)

--- server.js
+++ server.js
@@ -80,16 +80,8 @@ app.get('/', function (req, res) {
 app.get('/azure', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
+    console.log(res);
     res.send('{ pageCount: -1 }');
-  }
 });
 
 // error handling
