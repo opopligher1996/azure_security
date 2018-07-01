5f34160444cb5f587ea98522d0a5c257aa2cdb76
巴基亮 <pakistanleong@bajiliangde-MacBook-Pro.local>
2018-07-01 20:05:07 +0800

    delete 8080

--- server.js
+++ server.js
@@ -106,4 +106,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write(req); //write a response to the client
   res.end(); //end the response
-}).listen(8080);
+}).listen();
